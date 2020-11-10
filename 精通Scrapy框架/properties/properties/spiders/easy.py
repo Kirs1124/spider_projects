857cf97ec33cf92f7cc4a910e6f94e8041c04648
Kirs <1442947848@qq.com>
2020-11-10 17:33:48 +0800

    easy.py更新

--- 精通Scrapy框架/properties/properties/spiders/easy.py
+++ 精通Scrapy框架/properties/properties/spiders/easy.py
@@ -6,16 +6,17 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class EasySpider(CrawlSpider):
     name = 'easy'
-    allowed_domains = ['web']
-    start_urls = ['http://web/']
+    allowed_domains = ['nj.58.com']
+    start_urls = ['https://nj.58.com/ershoufang/?PGTID=0d30000c-00b9-5d1a-a6c8-7cc91b079351&ClickID=1']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//div[@id = "qySelectFirst"]/a[not(@href = "/ershoufang/")]')),
+        Rule(LinkExtractor(restrict_xpaths='//div[@class="pic"]/a'),callback = 'parse_item')
     )
 
     def parse_item(self, response):
         i = {}
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
+        # i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+        # i['name'] = response.xpath('//div[@id="name"]').extract()
+        # i['description'] = response.xpath('//div[@id="description"]').extract()
         return i
